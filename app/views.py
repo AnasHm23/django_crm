569dda85853a8542639881908db82e513041f6c8
Anashm24 <anashoumaid19@gmail.com>
2024-06-21 17:30:30 +0100

    django project

--- app/views.py
+++ app/views.py
@@ -1,4 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+
 
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.error(request, 'There Was An Error Loggin In ...')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Successfully Logged Out...")
+    return redirect('home')
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful! You are now logged in.")
+            return redirect('home')
+        else:
+            messages.error(request, "Registration failed. Please correct the errors below.")
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
